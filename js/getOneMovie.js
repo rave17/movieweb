63a3a9de16980d5b9182afb2ea5943e7f4099730
Roberto Vargas <rave17491@gmail.com>
2020-05-18 02:06:28 -0300

    get movie and localstorage

--- js/getOneMovie.js
+++ js/getOneMovie.js
@@ -1,11 +1,42 @@
-/* document.onclick =function(event){
-    const target = event.target;
-    
-        const movieID = target.dataset.movieId;
-        console.log(movieID)
-        const containerM = event.target.parentElement;
-        const content = containerM.nextElementSibling;
 
-        return console.log("oneMovie" + content);
-} 
- */
+$(document).ready(() =>{
+    showSome();
+    scrollWin(x, y);
+})
+ 
+function showSome(){
+    axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`)
+    .then((response) => {
+        let movies = response.data.results;
+      
+     
+        for(movie in movies){
+            let output = '';
+            
+            let urlTop = `${imgUrl}`+movies[movie].poster_path 
+            console.log(urlTop)
+            
+   
+            output += `
+           <img src=${urlTop} onclick="movieSelected('${movie.id}')" class="imgPointer" id="#moviePoster" alt=${movie.title}/>
+            `;
+
+         
+          $('#carouselMovies').append(output);
+        }
+
+    })  .catch((err) => {
+        console.log(err);
+      });
+  }
+
+
+  function scrollWin(x, y) {
+    $('#carouselMovies').scrollBy(x, y);
+  }
+
+
+
+
+
+
